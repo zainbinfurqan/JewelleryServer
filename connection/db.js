ea86ac452e28283316a5f62c58551d8984b9934d
vServices <VServices@vServices08.vservics.pk>
2020-01-23 15:17:55 +0500

    update

--- connection/db.js
+++ connection/db.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://zain:zainahmed@cluster0-scpx0.mongodb.net/jewelleryDB?retryWrites=true&w=majority";
+const URI = "mongodb+srv://zain:frgdwozZmdSpCk9s@cluster0-scpx0.mongodb.net/jewelleryDB?retryWrites=true&w=majority";
 const connectDB = async () => {
     await mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true })
     console.log("connnected...!")
 }
 module.exports = connectDB;
+// frgdwozZmdSpCk9s
